831340fa1e4579275310ef50975478c6f78bf47d
Joaquin <mozwho@gmail.com>
2019-09-07 13:12:02 -0300

    metodo constructorTest() en UsuarioTest()

--- facturas/src/test/java/com/api/facturas/modelos/UsuarioTest.java
+++ facturas/src/test/java/com/api/facturas/modelos/UsuarioTest.java
@@ -2,6 +2,10 @@ package com.api.facturas.modelos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 
 
 /** Para testear la clase Usuario
@@ -9,13 +13,29 @@ import static org.junit.jupiter.api.Assertions.*;
  * */
 
 class UsuarioTest {
+	
 	/**  Usuario para usar en varios test */
+	
+	Usuario usuarioTest;
+	
+	@BeforeClass
+	void setUp() { 
+		
+		/** seteado de usuarioTest para todas las pruebas */
+		
 	Usuario usuarioTest = new Usuario("nombre", "apellido", "nombreusuario", "contraseña", "correo", "direccion", 
             "ciudad", "estado", "pais", "codigopostal", "telefono");
+	}
 	
-	
-	
-	
+	@Test
+	void constructorTest() {
+		
+		/** test de constructor */
+		
+		assertTrue(usuarioTest instanceof Usuario);
+	}
+
+
 	
 	
 }
