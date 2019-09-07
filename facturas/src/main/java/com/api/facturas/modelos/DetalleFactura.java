99fc92946dc05cf496d79f33fd09e9bac90385fe
Andrés <trece920@hotmail.com>
2019-09-07 14:10:15 -0300

    Creación propiedades clase DetallesFactura

--- facturas/src/main/java/com/api/facturas/modelos/DetalleFactura.java
+++ facturas/src/main/java/com/api/facturas/modelos/DetalleFactura.java
@@ -1,9 +1,29 @@
 package com.api.facturas.modelos;
 
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
 /**
  * DetalleFactura
  */
-public class DetalleFactura {
-
+@Entity
+@Table(name = "detalle_factura")
+@EntityListeners(AuditingEntityListener.class)
+public class DetalleFactura implements Serializable{
+    
+    private static final long serialVersionUID = -732583211223219318L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_detalle_factura")
+    private Long idDetalleFactura;
+    @ManyToOne
+    private Factura factura;
+    @Column(name = "descripcion_proyecto")
+    private String descripcionProyecto;
+    @Column(name = "monto")
+    private double monto;
     
 }
