4560440fac60cc5fc1e6e670a337225ac5231b6f
Andrés <trece920@hotmail.com>
2019-09-07 18:07:41 -0300

    Creación queries repo usuarios

--- facturas/src/main/java/com/api/facturas/repositorios/RepositorioUsuarios.java
+++ facturas/src/main/java/com/api/facturas/repositorios/RepositorioUsuarios.java
@@ -1,9 +1,43 @@
 package com.api.facturas.repositorios;
 
-/**
- * RepositorioUsuarios
- */
-public interface RepositorioUsuarios {
+import com.api.facturas.modelos.Usuario;
 
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Interfaz RepositorioUsuarios
+ */
+public interface RepositorioUsuarios extends JpaRepository<Usuario, Long> {
+
+    /**
+     * Método que recibe un nombre de usuario y hace una consulta SQL para traer al
+     * usuario correspondiente y verificar si el nombre de usuario ya existe en la
+     * base de datos.
+     * 
+     * @param nombreUsuario
+     * @return Usuario
+     */
+    @Query(value = "SELECT * FROM  usuarios U WHERE U.nombre_usuario = :nombreUsuario", nativeQuery = true)
+    Usuario encontrarPorNombreUsuario(String nombreUsuario);
+
+    /**
+     * Método que recibe un correo electrónico y hace una consulta SQL para saber si
+     * el correo ya existe en la base de datos.
+     * 
+     * @param correo
+     * @return Correo electrónico
+     */
+    @Query(value = "SELECT U.correo_electronico FROM usuarios U WHERE U.correo_electronico = :correo", nativeQuery = true)
+    String correo(String correo);
+
+    /**
+     * Método que recibe un id y hace una consulta SQL para buscar a un usuario por
+     * su id y verificar si existe en la base de datos.
+     * 
+     * @param idUsuario
+     * @return
+     */
+    @Query(value = "SELECT * FROM usuarios U WHERE U.id_usuario = :idUsuario", nativeQuery = true)
+    Usuario encontrarPorId(Long idUsuario);
 }
