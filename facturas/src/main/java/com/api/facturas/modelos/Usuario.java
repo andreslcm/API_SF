3f6e7e5ac4dc8ad4c55890471b405be2b44c2fac
Andrés <trece920@hotmail.com>
2019-09-07 12:09:08 -0300

    creación de atributos

--- facturas/src/main/java/com/api/facturas/modelos/Usuario.java
+++ facturas/src/main/java/com/api/facturas/modelos/Usuario.java
@@ -1,7 +1,54 @@
 package com.api.facturas.modelos;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
 
 /**
  * Usuario
  */
-public class Usuario {
+@Entity
+@Table(name = "usuarios")
+@EntityListeners(AuditingEntityListener.class)
+public class Usuario implements Serializable{
+
+    private static final long serialVersionUID = -2887032741573862174L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+    @OneToMany(mappedBy = "usuario")
+    private List<Factura> facturas;
+    @OneToMany(mappedBy = "usuario")
+    private List<Cliente> clientes;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "nombre_usuario")
+    private String nombreUsuario;
+    @Column(name = "contrasena")
+    private String contrasena;
+    @Column(name = "correo_electronico")
+    private String correo;
+    @Column (name = "direccion")
+    private String direccion; 
+    @Column(name = "ciudad")
+    private String ciudad;
+    @Column(name = "estado")
+    private String estado;
+    @Column(name = "pais")
+    private String pais;
+    @Column(name = "codigo_postal")
+    private String codigoPostal;
+    @Column(name = "telefono")
+    private String telefono;
+    @Column(name = "medio_pago_1")
+    private String datosPaypal;
+    @Column(name = "medio_pago_2")
+    private String datosBanco;
+    @Column(name = "medio_pago_3")
+    private String datosPayoneer;
+    
+
 }
