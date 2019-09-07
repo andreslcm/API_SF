67af486a46c2810b4023f4236b53c98206dfce5d
Joaquin <mozwho@gmail.com>
2019-09-07 12:57:54 -0300

    variable Usuario para la clase UsuarioTest

--- facturas/src/test/java/com/api/facturas/modelos/UsuarioTest.java
+++ facturas/src/test/java/com/api/facturas/modelos/UsuarioTest.java
@@ -2,6 +2,20 @@ package com.api.facturas.modelos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UsuarioTest {
 
+
+/** Para testear la clase Usuario
+ * @version 06/10/19
+ * */
+
+class UsuarioTest {
+	/**  Usuario para usar en varios test */
+	Usuario usuarioTest = new Usuario("nombre", "apellido", "nombreusuario", "contraseña", "correo", "direccion", 
+            "ciudad", "estado", "pais", "codigopostal", "telefono");
+	
+	
+	
+	
+	
+	
 }
