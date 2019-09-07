d97420a4102e1b7db76fc97853c190225e967dae
Andrés <trece920@hotmail.com>
2019-09-07 11:21:21 -0300

    Documentación de la clase DemoApplication

--- facturas/src/main/java/com/api/facturas/DemoApplication.java
+++ facturas/src/main/java/com/api/facturas/DemoApplication.java
@@ -3,9 +3,16 @@ package com.api.facturas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Clase DemoApplication. A través de esta clase se ejecuta el proyecto de Spring.
+ */
 @SpringBootApplication
 public class DemoApplication {
 
+	/**
+	 * Método Main para correr la aplicación de Spring.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
