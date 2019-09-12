84d52dc3be9015ad7acaf8c0ad65e9e5512970ab
Andrés <trece920@hotmail.com>
2019-09-12 13:46:10 -0300

    Comentarios excepción

--- facturas/src/main/java/com/api/facturas/excepciones/RecursoNoEncontrado.java
+++ facturas/src/main/java/com/api/facturas/excepciones/RecursoNoEncontrado.java
@@ -7,11 +7,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * RecursoNoEncontrado
  */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class RecursoNoEncontrado extends RuntimeException{
+public class RecursoNoEncontrado extends RuntimeException {
 
     private static final long serialVersionUID = 6242482823488217664L;
-    public RecursoNoEncontrado (String mensaje){
+
+    /**
+     * Contructor que recibe el mensaje que se va a mostrar cuando se llame la
+     * excepción.
+     * 
+     * @param mensaje
+     */
+    public RecursoNoEncontrado(String mensaje) {
         super(mensaje);
     }
-    
 }
