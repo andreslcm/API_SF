cefa7fca9f58a9b781d00319925b5c5b129490ff
Andrés <trece920@hotmail.com>
2019-09-12 13:41:34 -0300

    Excepción recurso no encontrado

--- facturas/src/main/java/com/api/facturas/excepciones/RecursoNoEncontrado.java
+++ facturas/src/main/java/com/api/facturas/excepciones/RecursoNoEncontrado.java
@@ -1,7 +1,17 @@
 package com.api.facturas.excepciones;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * RecursoNoEncontrado
  */
-public class RecursoNoEncontrado {
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class RecursoNoEncontrado extends RuntimeException{
+
+    private static final long serialVersionUID = 6242482823488217664L;
+    public RecursoNoEncontrado (String mensaje){
+        super(mensaje);
+    }
+    
 }
