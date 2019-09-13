8fba17530be089782f96b129e6ed9bcf6324b3e5
Andrés <trece920@hotmail.com>
2019-09-13 19:13:49 -0300

    Creación de getters y setters de la Solicitud

--- facturas/src/main/java/com/api/facturas/seguridad/SolicitudJwt.java
+++ facturas/src/main/java/com/api/facturas/seguridad/SolicitudJwt.java
@@ -29,4 +29,39 @@ public class SolicitudJwt implements Serializable {
         this.contrasena = contrasena;
     }
 
+    /**
+     * Método para obtener el nombre de usuario.
+     * 
+     * @return {String} nombreUsuario
+     */
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    /**
+     * Método para asginar nombre de usuario.
+     * 
+     * @param nombreUsuario
+     */
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    /**
+     * Método para obtener la contraseña.
+     * 
+     * @return {String} contrasena
+     */
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    /**
+     * Método para asignar la contraseña.
+     * 
+     * @param contrasena
+     */
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 }
