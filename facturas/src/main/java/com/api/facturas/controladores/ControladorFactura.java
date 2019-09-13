845a6b7ce360492f9e1ec9f1fca853b7af53e557
Andrés <trece920@hotmail.com>
2019-09-13 11:40:45 -0300

    Método para agregar factura desde controlador

--- facturas/src/main/java/com/api/facturas/controladores/ControladorFactura.java
+++ facturas/src/main/java/com/api/facturas/controladores/ControladorFactura.java
@@ -1,8 +1,42 @@
 package com.api.facturas.controladores;
 
+import com.api.facturas.modelos.EnvoltorioFactura;
+import com.api.facturas.servicios.ServicioFactura;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * ControladorFactura
  */
 
+@RestController
+@CrossOrigin
 public class ControladorFactura {
+
+    @Autowired
+    private ServicioFactura servicio;
+
+    /**
+     * Método para agregar una factura a la BD.
+     * 
+     * @param envoltorio
+     * @param idUsuario
+     * @param idCliente
+     * @return
+     */
+    @PostMapping("crear-factura/{idUsuario}")
+    public ResponseEntity<?> agregarFactura(@RequestBody EnvoltorioFactura envoltorio,
+            @PathVariable(value = "idUsuario") Long idUsuario, @RequestParam(value = "idCliente") Long idCliente) {
+
+        servicio.agregarFactura(envoltorio, idUsuario, idCliente);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
