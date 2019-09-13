f7ee44d71b0483b769b60ffb701e4094c664d565
Andrés <trece920@hotmail.com>
2019-09-13 11:22:53 -0300

    Método para listar clientes desde controlador

--- facturas/src/main/java/com/api/facturas/controladores/ControladorCliente.java
+++ facturas/src/main/java/com/api/facturas/controladores/ControladorCliente.java
@@ -1,5 +1,7 @@
 package com.api.facturas.controladores;
 
+import java.util.List;
+
 import com.api.facturas.dtos.DtoCliente;
 import com.api.facturas.servicios.ServicioCliente;
 
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,8 +35,22 @@ public class ControladorCliente {
     @PostMapping(value = "/agregar-cliente/{idUsuario}")
     public ResponseEntity<?> agregarCliente(@RequestBody DtoCliente cliente,
             @PathVariable("idUsuario") Long idUsuario) {
+
         servicio.agregarCliente(cliente, idUsuario);
         return new ResponseEntity<>("OK", HttpStatus.CREATED);
+    }
+
+    /**
+     * Método para listar los clientes de un usuario.
+     * 
+     * @param idUsuario
+     * @return
+     */
+    @GetMapping("listar-clientes/{idUsuario}")
+    public ResponseEntity<?> listarCliente(@PathVariable("idUsuario") Long idUsuario) {
+
+        List<DtoCliente> listaClientes = servicio.listarClientes(idUsuario);
+        return new ResponseEntity<>(listaClientes, HttpStatus.OK);
     }
 
 }
