22c1ab94ca52760c9236689b5d3c0f5ae3407027
Andrés <trece920@hotmail.com>
2019-09-13 12:12:14 -0300

    Excepción correo existente

--- facturas/src/main/java/com/api/facturas/excepciones/CorreoExistente.java
+++ facturas/src/main/java/com/api/facturas/excepciones/CorreoExistente.java
@@ -1,9 +1,22 @@
 package com.api.facturas.excepciones;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * CorreoExistente
  */
-public class CorreoExistente {
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class CorreoExistente extends RuntimeException {
 
-    
+    private static final long serialVersionUID = 5264457827134874127L;
+
+    /**
+     * Constructor que recibe mensaje como parámetro.
+     * 
+     * @param mensaje
+     */
+    public CorreoExistente(String mensaje) {
+        super(mensaje);
+    }
 }
