25aa0bd32f1927a1477af064ea284e8116e9f556
Andrés <trece920@hotmail.com>
2019-09-13 19:48:20 -0300

    Método para obtener fecha vencimiento token

--- facturas/src/main/java/com/api/facturas/seguridad/HerramientaToken.java
+++ facturas/src/main/java/com/api/facturas/seguridad/HerramientaToken.java
@@ -9,19 +9,31 @@ import io.jsonwebtoken.Claims;
 /**
  * HerramientaToken
  */
-public class HerramientaToken implements Serializable{
+public class HerramientaToken implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    private static final long JWT_TOKEN_VALIDACION = 5 * 60 * 60;    
+    private static final long JWT_TOKEN_VALIDACION = 5 * 60 * 60;
     @Value("$jwt.secret")
     private String secreto;
 
-      /**
-       * Método para recuperar el nombre de usuario del token.
-       * @param token
-       * @return {String} obtenerClaimToken
-       */
-      public String getNombreUsuarioToken(String token){
+    /**
+     * Método para recuperar el nombre de usuario del token.
+     * 
+     * @param token
+     * @return {String} obtenerClaimToken
+     */
+    public String getNombreUsuarioToken(String token) {
         return obtenerClaimToken(token, Claims::getSubject);
     }
+
+    /**
+     * Método para recuperar
+     * 
+     * @param token
+     * @return {Date} obtenerClaimToken
+     */
+    public Date obtenerFechaVencimientoToken(String token) {
+        return obtenerClaimToken(token, Claims::getExpiration);
+    }
+
 }
