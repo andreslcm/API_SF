df271ad16473a4855ae03d5ddab36c415128904a
Andrés <trece920@hotmail.com>
2019-09-13 12:09:51 -0300

    Excepción usuario existente

--- facturas/src/main/java/com/api/facturas/excepciones/UsuarioExistente.java
+++ facturas/src/main/java/com/api/facturas/excepciones/UsuarioExistente.java
@@ -1,9 +1,22 @@
 package com.api.facturas.excepciones;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * UsuarioExistente
  */
-public class UsuarioExistente {
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class UsuarioExistente extends RuntimeException {
 
-    
+    private static final long serialVersionUID = -6862945501201879865L;
+
+    /**
+     * Constructor que recibe mensaje como parámetro.
+     * 
+     * @param mensaje
+     */
+    public UsuarioExistente(String mensaje) {
+        super(mensaje);
+    }
 }
