5591a4820ae5c9e75ba8b45ca73fbed979e4bd74
Joaquin <mozwho@gmail.com>
2019-10-07 00:03:29 -0300

    metodo crearUsuarioTest() en ServicioUsuarioTest

--- facturas/src/test/java/com/api/facturas/servicios/ServicioUsuarioTest.java
+++ facturas/src/test/java/com/api/facturas/servicios/ServicioUsuarioTest.java
@@ -2,6 +2,51 @@ package com.api.facturas.servicios;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.api.facturas.dtos.DtoUsuario;
+import com.api.facturas.modelos.Cliente;
+import com.api.facturas.modelos.Factura;
+import com.api.facturas.modelos.Usuario;
+
 class ServicioUsuarioTest {
 
+	/**  ServicioUsuario para usar en varios test */
+	
+	ServicioUsuario servicioTest;
+	DtoUsuario usuarioTest;
+	
+	@BeforeClass
+	void setUp() { 
+		
+	/**  setUp de DtoUsuario para usar en los test  */
+		
+		usuarioTest = new DtoUsuario((long) 117, "testn", "testa", "testnu","testc","testcor","testdir","testcid","testes","testpa","testcod","testtel","testdb","testdp","testpayoneer"));
+		
+	}
+	
+	@Test
+	void crearUsuarioTest() {
+		
+		/** test de ServicioUsuario.crearUsuario() */
+		
+		Usuario testy = servicioTest.crearusUario(usuarioTest);
+		
+		assertNotNull(testy);
+		assertTrue(testy instanceof Usuario);
+		assertTrue(testy.equals(new Usuario(usuarioTest)));
+	}
+	
+	
+
+
+	
+	
+	
+	
+	
 }
