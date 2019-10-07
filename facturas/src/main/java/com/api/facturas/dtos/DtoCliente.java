9c674d20afed0b3070a24aa7b012501cde4636d7
Joaquin <mozwho@gmail.com>
2019-10-07 13:51:23 -0300

    nuevo contrusctor en DtoCliente para prosositos de testeo

--- facturas/src/main/java/com/api/facturas/dtos/DtoCliente.java
+++ facturas/src/main/java/com/api/facturas/dtos/DtoCliente.java
@@ -23,6 +23,13 @@ public class DtoCliente {
     private double horaEdicion;
     private double palabraProofreading;
     private double horaProofreading;
+    
+    
+    
+    
+    
+    
+    
 
     /**
      * Constructor vacío
@@ -53,6 +60,41 @@ public class DtoCliente {
         this.horaTraduccion = cliente.getHoraTraduccion();
         this.horaEdicion = cliente.getHoraEdicion();
         this.horaProofreading = cliente.getHoraProofreading();
+    }
+    
+    
+    /**
+     * Constructor para propositos de testeo
+     * 
+     * 
+     * @param id,nombre,direccion,ciudad,estado,pais,codpos,tel,corel,termpago,
+     * paltrac,paled,palproo,horatra,horaed,horaproo
+     */
+    
+    
+    public DtoCliente(Long id, String nombre, String direccion,
+    		String ciudad, String estado, String pais, String codpos,
+    		String tel, String corel, int termpago, Double paltrac,
+    		Double paled, Double palproo, Double horatra, Double horaed, Double horaproo) {
+    	
+        this.idCliente = id;
+        this.nombreCliente = nombre;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+        this.estado = estado;
+        this.pais = pais;
+        this.codigoPostal = codpos;
+        this.telefono = tel;
+        this.correoElectronico = corel;
+        this.terminoPago = termpago;
+        this.palabraTraduccion = paltrac;
+        this.palabraEdicion = paled;
+        this.palabraProofreading = palproo;
+        this.horaTraduccion = horatra;
+        this.horaEdicion = horaed;
+        this.horaProofreading = horaproo;
+    	
+    
     }
 
     /**
