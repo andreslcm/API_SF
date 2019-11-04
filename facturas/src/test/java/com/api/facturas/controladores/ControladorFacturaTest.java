457da0d09c0725bf945849b6993c0ba17ad93f6b
Joaquin <mozwho@gmail.com>
2019-11-04 14:56:39 -0300

    metodos agregarFacturaTest(), listarFacturaTest(), listarFacturaPorClienteTest(), modificarFacturaTest(), eliminarFacturaTest() y pagarFacturaTest() en ControladorFacturaTest

--- facturas/src/test/java/com/api/facturas/controladores/ControladorFacturaTest.java
+++ facturas/src/test/java/com/api/facturas/controladores/ControladorFacturaTest.java
@@ -2,6 +2,173 @@ package com.api.facturas.controladores;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.api.facturas.dtos.ClienteDtoTester;
+import com.api.facturas.dtos.DtoCliente;
+import com.api.facturas.dtos.DtoDetalleFactura;
+import com.api.facturas.dtos.DtoFactura;
+import com.api.facturas.dtos.DtoUsuario;
+import com.api.facturas.dtos.EnvoltorioTester;
+import com.api.facturas.dtos.FacturaDtoTester;
+import com.api.facturas.dtos.UsuarioDtoTester;
+import com.api.facturas.modelos.EnvoltorioFactura;
+import com.api.facturas.servicios.ServicioCliente;
+import com.api.facturas.servicios.ServicioFactura;
+import com.api.facturas.servicios.ServicioUsuario;
+
 class ControladorFacturaTest {
+	
+	
+	
+	/**  ControladorFacturaTest para usar en varios test */
+	
+	ControladorFactura test;
+	ServicioCliente serviciotest;
+	DtoCliente dto;
+	DtoUsuario dto2;
+	DtoFactura dto3;
+	EnvoltorioFactura dto4;
+	DtoFactura dto5;
+	List<DtoDetalleFactura> lista;
+	ServicioUsuario serviciotest2;
+	ServicioFactura serviciotest3;
+	
+	
+	@BeforeClass
+	void setUp() { 
+		
+	/**  setUp para usar en los test  */
+		
+		dto2 = new UsuarioDtoTester((long)117, "testn", "testa",
+				"testnu","testc","testcor","testdir","testcid","testes",
+				"testpa","testcod","testtel","testdb","testdp",
+				"testpayoneer");
+		
+		dto = new ClienteDtoTester((long)44, "testn","testdir",
+				"testc", "teste","testp", "testcod", "testtel",
+				"testcorel", 999, 999.0, 999.0,
+				999.0, 999.0, 999.0, 999.0);
+		
+		Date datetest = new Date();
+		lista = new ArrayList<>();
+		
+		dto3 = new FacturaDtoTester((long)77, (long)77, "orden", datetest,
+				datetest, 77.0, 77.0, 77.0,
+				"notas", false );
+		
+		dto4 = new EnvoltorioTester(dto3, lista);
+		
+		dto5 = new FacturaDtoTester((long)77, (long)77, "orden", datetest,
+				datetest, 77.0, 77.0, 77.0,
+				"notas", true );
+		
+		serviciotest2.crearUsuario(dto2);
+		serviciotest.agregarCliente(dto, (long)117);
+		
+		
+	}
+	
+	@Test
+	void agregarFacturaTest() {
+		
+		/**  test de Controladorfactura.agregarFactura()
+		 * @param dto 
+		 * @param long*/
+		
+		test.agregarFactura(dto4, (long)117, (long)44);
+		assertTrue(enLista(serviciotest3.listarFacturas((long)117), dto3));
+
+	}
+
+	@Test
+	void listarFacturaTest() {
+		
+		/**  test de ControladorFactura.listarFactura()
+		 * 
+		 * @param dto 
+		 * @param long*/
+		
+
+		assertTrue(enLista(test.listarFacturas((long)117), dto3));
+
+	}
+	
+	@Test
+	void listarFacturaPorClienteTest() {
+		
+		/**  test de ControladorFactura.listarFacturaPorCliente()
+		 * 
+		 * @param dto 
+		 * @param long*/
+		
+
+		assertTrue(enLista(test.listarFacturasPorCliente((long)44), dto3));
+
+	}
+	
+	
+	@Test
+	void modificarFacturaTest() {
+		
+		/**  test de ControladorFactura.modificarFactura()
+		 * 
+		 * @param dto5
+		 * @param dto4
+		 * @param long*/
+		
+		dto4.setFactura(dto5);
+
+		test.modificarFactura(dto4, (long)77);
+		assertTrue(enLista(test.listarFacturas((long)117), dto5));
+
+	}
+	
+	@Test
+	void eliminarFacturaTest() {
+		
+		/**  test de ControladorFactura.eliminarFactura()
+		 * 
+		 * @param long*/
+	
+
+		test.eliminarFactura((long)77);
+		assertTrue(!enLista(test.listarFacturas((long)117), dto3));
+
+	}
+	
+	@Test
+	void pagarFacturaTest() {
+		
+		/**  test de ControladorFactura.pagarFactura()
+		 * 
+		 * @param long*/
+	
+
+		test.pagarFactura((long)77);
+		assertTrue(enLista(test.listarFacturas((long)117), dto5));
+
+	}
+	
+	
+	
+	public boolean enLista(List<DtoFactura> lista, DtoFactura dto) {
+		
+		/** metodo para asistir en los @test
+		 * @param lista
+		 * lista de DtoCliente */
+		
+		for (DtoFactura factura : lista) {
+			if (factura.equals(dto)) return true;
+		}
+		return false;
+	}
+
 
 }
+	
